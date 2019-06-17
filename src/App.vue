540fab43567f447a35655a716b76550f2376f07c
shitou <2832904090@qq.com>
2019-06-18 01:02:26 +0800

    aaa

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="player">
+        <video :src="src"></video>
+        <div class="player__watermark" v-if="watermark">{{watermark}}</div>
+        <div class="palyer__title" v-if="title" v-show="status">{{title}}</div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+      name: 'EasyVideoPlayer',
+      props: {
+        src: String,
+        watermark: [String, Number],
+        title: [String, Number]
+      },
+      data () {
+        return {
+          status: ''
+        }
+      }
+    }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped src="./css/index.css"></style>
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import EasyVideoPlayer from './App'
+
+EasyVideoPlayer.install = function (Vue) {
+  Vue.component('easy-video-player', EasyVideoPlayer)
+}
+
+export default EasyVideoPlayer
